8eaf554c107da255710e01a086a7360c01c59ded
pLogicador <pedroemanuelsm0@gmail.com>
2025-04-03 18:47:23 -0300

    feat: add not-found page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ async function getData() {
 async function getData() {
   const data = await delayFetch(
     //https://api.github.com/users/plogicador/repos
-    "https://api.github.com/ueRr/plogicador/repos",
+    "https://api.github.com/users/plogicador/repos",
     0
   );
   return data;
